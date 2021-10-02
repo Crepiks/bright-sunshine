4e91c5a00511e08220b6df4ce32aa961c345d10c
Crepiks <sayazhan.onlassyn@mail.ru>
2021-10-02 21:25:57 +0600

    feat: create solar chart component for home screen

---
+++ lib/home/views/components/solar_chart.dart
@@ -0,0 +1,215 @@
+import 'dart:async';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:sunshine/common/colors.dart';
+
+class SolarChart extends StatefulWidget {
+  const SolarChart({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => BarChartSample1State();
+}
+
+class BarChartSample1State extends State<SolarChart> {
+  final Color barBackgroundColor = Colors.white;
+  final Duration animDuration = const Duration(milliseconds: 250);
+
+  int touchedIndex = -1;
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1,
+      child: Container(
+        decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(20))),
+        child: Stack(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  const Text(
+                    'Week',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 22,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: BarChart(
+                        mainBarData(),
+                        swapAnimationDuration: animDuration,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  BarChartGroupData makeGroupData(
+    int x,
+    double y, {
+    bool isTouched = false,
+    Color barColor = primaryColor,
+    double width = 20,
+    List<int> showTooltips = const [],
+  }) {
+    return BarChartGroupData(
+      x: x,
+      barRods: [
+        BarChartRodData(
+            y: y,
+            colors: isTouched ? [Colors.black] : [barColor],
+            width: width,
+            borderRadius: const BorderRadius.all(Radius.circular(6))),
+      ],
+      showingTooltipIndicators: showTooltips,
+    );
+  }
+
+  List<BarChartGroupData> showingGroups() => List.generate(7, (i) {
+        switch (i) {
+          case 0:
+            return makeGroupData(0, 5, isTouched: i == touchedIndex);
+          case 1:
+            return makeGroupData(1, 6.5, isTouched: i == touchedIndex);
+          case 2:
+            return makeGroupData(2, 5, isTouched: i == touchedIndex);
+          case 3:
+            return makeGroupData(3, 7.5, isTouched: i == touchedIndex);
+          case 4:
+            return makeGroupData(4, 9, isTouched: i == touchedIndex);
+          case 5:
+            return makeGroupData(5, 11.5, isTouched: i == touchedIndex);
+          case 6:
+            return makeGroupData(6, 6.5, isTouched: i == touchedIndex);
+          default:
+            return throw Error();
+        }
+      });
+
+  BarChartData mainBarData() {
+    return BarChartData(
+      barTouchData: BarTouchData(
+        touchTooltipData: BarTouchTooltipData(
+            getTooltipItem: (group, groupIndex, rod, rodIndex) {
+          String weekDay;
+          switch (group.x.toInt()) {
+            case 0:
+              weekDay = 'Monday 19.09';
+              break;
+            case 1:
+              weekDay = 'Tuesday 20.09';
+              break;
+            case 2:
+              weekDay = 'Wednesday 21.09';
+              break;
+            case 3:
+              weekDay = 'Thursday 22.09';
+              break;
+            case 4:
+              weekDay = 'Friday 23.09';
+              break;
+            case 5:
+              weekDay = 'Saturday 24.09';
+              break;
+            case 6:
+              weekDay = 'Sunday 25.09';
+              break;
+            default:
+              throw Error();
+          }
+          return BarTooltipItem(
+            weekDay + '\n',
+            const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 14,
+            ),
+            children: <TextSpan>[
+              TextSpan(
+                text: rod.y.toString(),
+                style: const TextStyle(
+                  color: primaryColor,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          );
+        }),
+        touchCallback: (FlTouchEvent event, barTouchResponse) {
+          setState(() {
+            if (!event.isInterestedForInteractions ||
+                barTouchResponse == null ||
+                barTouchResponse.spot == null) {
+              touchedIndex = -1;
+              return;
+            }
+            touchedIndex = barTouchResponse.spot!.touchedBarGroupIndex;
+          });
+        },
+      ),
+      titlesData: FlTitlesData(
+        show: true,
+        rightTitles: SideTitles(showTitles: false),
+        topTitles: SideTitles(showTitles: false),
+        bottomTitles: SideTitles(
+          showTitles: true,
+          getTextStyles: (context, value) => const TextStyle(
+              color: Colors.black, fontWeight: FontWeight.w500, fontSize: 14),
+          margin: 16,
+          getTitles: (double value) {
+            switch (value.toInt()) {
+              case 0:
+                return 'Mon';
+              case 1:
+                return 'Tue';
+              case 2:
+                return 'Wed';
+              case 3:
+                return 'Thu';
+              case 4:
+                return 'Fri';
+              case 5:
+                return 'Sat';
+              case 6:
+                return 'Sun';
+              default:
+                return '';
+            }
+          },
+        ),
+        leftTitles: SideTitles(
+          showTitles: false,
+        ),
+      ),
+      borderData: FlBorderData(
+        show: false,
+      ),
+      barGroups: showingGroups(),
+      gridData: FlGridData(show: false),
+    );
+  }
+
+  Future<dynamic> refreshState() async {
+    setState(() {});
+    await Future<dynamic>.delayed(
+        animDuration + const Duration(milliseconds: 50));
+  }
+}
--- lib/home/views/home_view.dart
+++ lib/home/views/home_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:sunshine/common/colors.dart';
+import 'package:sunshine/home/views/components/solar_chart.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -13,19 +15,40 @@ class HomeView extends StatelessWidget {
             child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
           child: Column(children: [
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: const [
-                  Text(
-                    "Sunshine",
-                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.w700),
-                  ),
-                  Text(
-                    "Алматы",
-                    style: TextStyle(fontSize: 16),
-                  )
-                ])
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Sunshine",
+                      style:
+                          TextStyle(fontSize: 28, fontWeight: FontWeight.w700),
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const [
+                        FaIcon(
+                          FontAwesomeIcons.mapMarkerAlt,
+                          size: 18,
+                        ),
+                        SizedBox(
+                          width: 6,
+                        ),
+                        Text(
+                          "Almaty",
+                          style: TextStyle(fontSize: 16),
+                        )
+                      ],
+                    )
+                  ]),
+            ),
+            const SizedBox(
+              width: double.infinity,
+              height: 300,
+              child: SolarChart(),
+            )
           ]),
         )));
   }
